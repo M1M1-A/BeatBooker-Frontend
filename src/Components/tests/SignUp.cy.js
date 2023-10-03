921c1010bba8b5466d2cf98c9e23d0c2770b7463
Lydia Ntafa <you@example.comlydiadafa@gmail.com>
2023-10-03 16:20:51 +0100

    fixed tests after last merge

--- src/Components/tests/SignUp.cy.js
+++ src/Components/tests/SignUp.cy.js
@@ -7,18 +7,18 @@ describe('signup Component', () => {
   });
 
   it('renders the signup form', () => {
-      cy.get('[data-testid="name"]').should('be.visible')
+      cy.get('[data-testid="username"]').should('be.visible')
       cy.get('[data-testid="password"]').should('be.visible')
   });
     it('shows an error message and the signup button is disabled when the passwords do not match', () => {
-        cy.get('[data-testid=name-input]').type("someone");
+        cy.get('[data-testid=username-input]').type("someone");
         cy.get('[data-testid=password-input]').type("password");
         cy.get('[data-testid=confirm_password-input]').type("notpassword");
         expect('[data-testid=password_error]').to.exist;
         expect(cy.get('#signup-button').should('be.disabled'));
     });
     it('shows an error message when the password is invalid', () => {
-        cy.get('[data-testid=name-input]').type("someone");
+        cy.get('[data-testid=username-input]').type("someone");
         cy.get('[data-testid=password-input]').type("pass");
         cy.get('[data-testid=confirm_password-input]').type("pass");
         cy.get('[data-testid=email-input]').type("someone@someone.com");
